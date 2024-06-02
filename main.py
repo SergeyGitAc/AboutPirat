515f54d7a024610bbf2da5e7051f7f4f66efebba
SergeyAcGit <sergey.acc.main@gmail.com>
2024-06-02 19:18:27 +0300

    Seve

--- main.py
+++ main.py
@@ -5,9 +5,41 @@ app = Flask(__name__)
 @app.route('/')
 def start():
     return render_template("start.html")
+
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about_me.html")
+
+@app.route('/m1')
+def m1():
+    return render_template("m1.html")
+
+@app.route('/m2')
+def m2():
+    return render_template("m2.html")
+
+@app.route('/m3')
+def m3():
+    return render_template("m3.html")
+
+@app.route('/m4')
+def m4():
+    return render_template("m4.html")
+
+@app.route('/m5')
+def m5():
+    return render_template("m5.html")
+
+@app.route('/m6')
+def m6():
+    return render_template("m6.html")
+
+
+
 
 if __name__ == "__main__":
     app.run()
